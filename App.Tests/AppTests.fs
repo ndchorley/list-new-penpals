e4b8b2eddd293075b165225dba00a1a0dfc4a6f0
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-03-22 16:41:27 +0000

    Remove some unnecessary parens

--- App.Tests/AppTests.fs
+++ App.Tests/AppTests.fs
@@ -16,8 +16,8 @@ let It_displays_a_message_when_there_are_no_penpals_to_write_to () =
 
     Assert.That(
         writer.ToString(),
-        Is.EqualTo(
+        Is.EqualTo
             "\x1B[0;35m>>\x1B[0m \
             You have no penpals to write to\n\
-            \x1B[0;35m>>\x1B[0m ")
+            \x1B[0;35m>>\x1B[0m "
     )
