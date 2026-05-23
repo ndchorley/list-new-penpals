3d92dbb46bff3f6ff70fba6e2e8d8ee7b5ed2be0
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-05-23 11:32:33 +0100

    Add the beginning of a script to import from the file

---
+++ file-importer/Program.fs
@@ -0,0 +1,16 @@
+﻿let removeHeader lines =
+    Seq.skip 2 lines
+
+let onlyThoseWhoHaventBeenWrittenTo lines =
+    Seq.filter
+        (fun (line: string) -> not (line.EndsWith("|x")))
+        lines
+
+let fileName =
+    System.Environment.GetEnvironmentVariable("HOME")
+    + "/penpal_list"
+
+System.IO.File.ReadLines fileName
+|> removeHeader
+|> onlyThoseWhoHaventBeenWrittenTo
+|> Seq.iter (fun line -> printfn $"{line}")
