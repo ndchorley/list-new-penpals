9eb658bd985ad00de3e8804736d700414b4ce013
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-05-23 13:17:00 +0100

    Now insert the penpals into the database

--- file-importer/Program.fs
+++ file-importer/Program.fs
@@ -25,6 +25,32 @@ let toPenpals lines =
         )
         lines
 
+let insertIntoDatabase penpals =
+    let dbFile = System.Environment.GetEnvironmentVariable("HOME") + "/penpal_list.db"
+    let connectionString = "Data Source = " + dbFile
+    let connection = new Microsoft.Data.Sqlite.SqliteConnection(connectionString)
+    connection.Open()
+
+    Seq.iter
+        (fun penpal ->
+            let command =
+                new Microsoft.Data.Sqlite.SqliteCommand(
+                    "INSERT INTO Penpals VALUES ($name, $address, $languages);", 
+                    connection
+                )
+
+            command.Parameters.AddWithValue("$name", penpal.name)
+            command.Parameters.AddWithValue("$address", penpal.address)
+            command.Parameters.AddWithValue("languages", penpal.languages)
+
+            command.ExecuteNonQuery()
+
+            ()
+        )
+        penpals
+
+    connection.Close()
+
 let fileName =
     System.Environment.GetEnvironmentVariable("HOME")
     + "/penpal_list"
@@ -33,4 +59,4 @@ System.IO.File.ReadLines fileName
 |> removeHeader
 |> onlyThoseWhoHaventBeenWrittenTo
 |> toPenpals
-|> Seq.iter (fun penpal -> printfn $"{penpal}")
+|> insertIntoDatabase
