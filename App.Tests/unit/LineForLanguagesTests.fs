f65b13e9704c7647c2e9ed68e75bf4b8d05300c0
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-07-31 13:12:03 +0100

    Format more than two languages nicely

--- App.Tests/unit/LineForLanguagesTests.fs
+++ App.Tests/unit/LineForLanguagesTests.fs
@@ -20,3 +20,12 @@ let It_separates_two_languages_by_and () =
        lineFor languages,
        Is.EqualTo "who writes in French and English"
     )
+
+[<Test>]
+let It_separates_more_than_two_languages_by_commas_and_an_and_between_the_last_two () =
+    let languages = ["French"; "German"; "English"; "Portuguese"]
+
+    Assert.That (
+        lineFor languages,
+        Is.EqualTo "who writes in French, German, English and Portuguese"
+    )
--- App/Display.fs
+++ App/Display.fs
@@ -4,11 +4,16 @@ let lineFor (languages: string list) =
     let prefix = "who writes in "
 
     let rest =
-        if languages.Length = 1 then
-            languages.Head
-        elif languages.Length = 2 then
-            languages.Head + " and " + languages.Tail.Head
-        else
-            languages.Head + ", " + languages.Tail.Head
+        languages
+        |> List.indexed
+        |> List.fold
+               (fun soFar (index, language) ->
+                    let separator =
+                        if index = languages.Length - 1 then ""
+                        elif index = languages.Length - 2 then " and "
+                        else ", "
+
+                    soFar + language + separator
+                ) ""
 
     prefix + rest
