d2c9603e1bc30450be408f01d292e26702a81a61
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-07-31 10:48:53 +0100

    Refactor lineFor to improve readability

--- App/Display.fs
+++ App/Display.fs
@@ -3,15 +3,12 @@ module Display
 let lineFor (languages: string list) =
     let prefix = "who writes in "
 
-    if languages.Length = 1 then
-        prefix + languages.Head
-    elif languages.Length = 2 then
-        prefix
-        + languages.Head
-        + " and "
-        + languages.Tail.Head
-    else
-        prefix
-        + languages.Head
-        + ", "
-        + languages.Tail.Head
+    let rest =
+        if languages.Length = 1 then
+            languages.Head
+        elif languages.Length = 2 then
+            languages.Head + " and " + languages.Tail.Head
+        else
+            languages.Head + ", " + languages.Tail.Head
+
+    prefix + rest
