7e42e615134fb114fc62e402db9799a6ff9d3ca3
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2026-07-31 14:40:01 +0100

    Extract a function for listing penpals

--- App/App.fs
+++ App/App.fs
@@ -4,6 +4,18 @@ open Display
 open Penpal
 open System.IO
 
+let private list (outputWriter: TextWriter) (penpals: Penpal list) =
+    if penpals.IsEmpty then
+        outputWriter.WriteLine "You have no penpals to write to"
+    else
+        let penpal = penpals.Head
+
+        outputWriter.WriteLine "You have not written to:"
+        outputWriter.WriteLine penpal.name
+        outputWriter.WriteLine penpal.address
+        outputWriter.WriteLine ""
+        outputWriter.WriteLine (lineFor penpal.languages)
+
 let run (inputReader: TextReader) (outputWriter: TextWriter) (penpals: Penpal list) =
     let rec loop () =
         outputWriter.Write "\x1B[0;35m>>\x1B[0m "
@@ -11,18 +23,7 @@ let run (inputReader: TextReader) (outputWriter: TextWriter) (penpals: Penpal li
 
         if commandString = "Q" then ()
         elif commandString = "L" then
-            if penpals.IsEmpty then
-                outputWriter.WriteLine "You have no penpals to write to"
-            else
-                let penpal = penpals.Head
-
-                outputWriter.WriteLine "You have not written to:"
-                outputWriter.WriteLine penpal.name
-                outputWriter.WriteLine penpal.address
-                outputWriter.WriteLine ""
-                outputWriter.WriteLine (lineFor penpal.languages)
-                
-                ()
+            list outputWriter penpals
 
             loop ()
         else
